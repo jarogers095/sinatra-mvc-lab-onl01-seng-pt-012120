0b3e14f7d0c8fbd77b2e9460840e9300a6fda1c5
James Rogers <jarogers095@gmail.com>
2020-04-16 04:42:58 +0000

    Automatically backed up by Learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,16 @@
+class PigLatinizer
+  attr_reader :text, :result
+  
+  def initialize(text)
+    @text = text
+    process_text
+  end
+  
+  def process_text
+    text_array = @text.split(" ")
+    latin_array = text_array.map do |word|
+      
+    end
+  end
+  
+end
