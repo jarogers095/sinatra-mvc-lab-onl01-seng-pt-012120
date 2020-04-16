2b236a5cae6cae2096c3921ecd3b7e25b354d235
James Rogers <jarogers095@gmail.com>
2020-04-16 05:23:19 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,24 +5,23 @@ class PigLatinizer
 
   end
   
-  def process_text
-    text_array = @text.split(" ")
-    
-    latin_array = text_array.map do |word|
-      if word[0].downcase == "y"
-        first_vowel = word.index(/[aeiou]/)
-      else
-        first_vowel = word.index(/[aeiouy]/)
-      end
-      
-      if first_vowel == 0 
-        word + "way"
-      else
-        word.slice!(first_vowel..-1) + word + "ay"
-      end
+  def piglatinize(word)
+    if word[0].downcase == "y"
+      first_vowel = word.index(/[aeiou]/)
+    else
+      first_vowel = word.index(/[aeiouy]/)
     end
-
-    @result = latin_array.join(" ")
+    
+    if first_vowel == 0 
+      word + "way"
+    else
+      word.slice!(first_vowel..-1) + word + "ay"
+    end
   end
   
+  #def split
+  #  text_array = @text.split(" ")
+  #  
+  #  latin_array = text_array.map do |word|
+  #end
 end
